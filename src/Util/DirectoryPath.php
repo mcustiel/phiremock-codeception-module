f76b44f34e08bb3f0be15b43c2bf901ffa74b5f2
Mariano Custiel <jmcustiel@gmail.com>
2021-02-17 21:02:33 +0100

    Trying to fix the regex

--- src/Util/DirectoryPath.php
+++ src/Util/DirectoryPath.php
@@ -55,7 +55,7 @@ class DirectoryPath
     /** @throws ConfigurationException */
     public static function createAbsoluteOrRelativeToCodeceptionDir(string $path): self
     {
-        if (preg_match('%^(?:[a-z]:\\|/)%', $path) === 1) {
+        if (preg_match('%^(?:[a-z]:\|/)%', $path) === 1) {
             return new self($path);
         }
         return new self(Configuration::projectDir() . $path);
